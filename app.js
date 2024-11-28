9f80c5bcdc2aab4f5902ee4475655773673f19f3
JhoselPR <112789135+JhoselPR@users.noreply.github.com>
2024-11-28 10:53:13 -0600

    ejercicio resuelto

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// Escribe tu código aquí.
+
+const bigWords = (word, words) => {
+    return words.filter((e) => e.length > word.length);
+}
+
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+
+const arr = bigWords('bocina', myArray);
+console.log(arr);
+
+const printList = (arr) => {
+    const lista = document.querySelector("#lista");
+    arr.forEach(e => {
+        let item = document.createElement("li");
+        item.textContent = e;
+        lista.appendChild(item);
+    });
+}
+
+
+
+const btn = document.querySelector(".trigger");
+btn.addEventListener("click", () => {
+    let userInput = prompt("Please enter a word");
+    let newList = bigWords(userInput, myArray);
+    printList(newList);
+})
